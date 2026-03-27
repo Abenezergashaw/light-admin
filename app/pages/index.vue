5dad25b54d078e2ba725bd695f5bcd24eec85cee
cherent tsegaye <abenigas59@gmail.com>
2026-03-27 21:54:50 +0300

    first commit

---
+++ app/pages/index.vue
@@ -0,0 +1,72 @@
+<script setup>
+const { call } = useApi();
+const { user } = useAuth();
+
+const totalData = ref(null);
+
+const range = ref("day");
+
+const fetchReport = async (range) => {
+  const res = await call({
+    url: "/api/financial-report",
+    method: "POST",
+    data: {
+      range,
+      userId: user?.value?.id,
+    },
+    // credentials: true,
+  });
+
+  totalData.value = res;
+};
+
+watch(
+  () => user.value,
+  async (newUser) => {
+    if (newUser && newUser.id) {
+      await fetchReport("day");
+    }
+  },
+  { immediate: true }, // Run immediately in case user is already there
+);
+
+const handleFilter = async (range) => {
+  await fetchReport(range);
+};
+
+const handleRange = async (r) => {
+  range.value = r;
+  console.log(r, "rrrr");
+  await fetchReport(r);
+};
+
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "ETB", // Change to 'ETB', 'EUR', etc.
+});
+</script>
+
+<template>
+  <div class="p-2 space-y-6">
+    <div class="flex justify-between items-center">
+      <div class="bg-green-100 text-green-900 p-3 rounded-xl">
+        {{ user?.username }}
+      </div>
+      <div class="bg-orange-200 text-orange-900 p-3 rounded-xl">
+        {{ formatter.format(user?.rBalance || 0) }}
+      </div>
+    </div>
+    <GgrReport
+      v-if="totalData && user?.role === 'admin'"
+      :report="totalData.report"
+      @changeFilter="handleFilter"
+    />
+
+    <FinancialReport
+      v-if="totalData && user?.role === 'agent'"
+      :report="totalData.report"
+      :range="range"
+      @changeRange="handleRange"
+    />
+  </div>
+</template>
---
+++ app/pages/users/[user].vue
@@ -0,0 +1,45 @@
+<script setup>
+const { call } = useApi();
+
+const report = ref(null);
+const range = ref("day");
+
+const route = useRoute();
+
+const user = route.params.user;
+console.log(user);
+
+const fetchReport = async () => {
+  const res = await call({
+    url: "/api/financial-report",
+    method: "POST",
+    data: {
+      userId: user,
+      range: range.value,
+    },
+    credentials: true,
+  });
+
+  if (!res.error) {
+    report.value = res.report;
+  }
+};
+
+await fetchReport();
+
+const handleRange = async (r) => {
+  range.value = r;
+  await fetchReport();
+};
+</script>
+
+<template>
+  <div class="p-4 space-y-4">
+    <FinancialReport
+      v-if="report"
+      :report="report"
+      :range="range"
+      @changeRange="handleRange"
+    />
+  </div>
+</template>
